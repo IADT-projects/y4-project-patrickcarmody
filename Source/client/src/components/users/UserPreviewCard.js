6abee9a07480689c4c96fe1bc0527af06651e5bb
patrickcarmody <pcarmody00@gmail.com>
2023-04-06 18:30:53 +0100

    Update UserPreviewCard.js

--- Source/client/src/components/users/UserPreviewCard.js
+++ Source/client/src/components/users/UserPreviewCard.js
@@ -26,24 +26,24 @@ const UserPreviewCard = ({ address }) => {
         <Card>
             <Grid container display={'flex'}>
                 <Grid item xs={12} padding={2}>
-                    <Typography variant="h4" textAlign={'center'}>
+                    <Typography variant="h5" textAlign={'center'} paddingY={2}>
                         Creator
                     </Typography>
                 </Grid>
                 <Grid item xs={6} sx={{height: 150}}>
                     <CardMedia
-                        sx={{ height: '100px', width: '100px', borderRadius: '80%', marginX: 'auto'}}
+                        sx={{ height: '120px', width: '120px', borderRadius: '80%', marginX: 'auto'}}
                         image={`https://res.cloudinary.com/dzooewr3a/image/upload/${creator.image}.png`}
                     />
                 </Grid>
                 <Grid item container xs={6}>
                     <Grid item xs={12}>
-                        <Typography variant="h6" textAlign={'center'}>
+                        <Typography sx={{ textAlign: 'center', fontWeight: 'bold', fontSize: 16 }}>
                             {creator.first_name} {creator.last_name}
                         </Typography>
                     </Grid>
                     <Grid item xs={6}>
-                        <Typography variant="body1" textAlign={'center'} sx={{fontWeight: 'medium'}}>
+                        <Typography sx={{ textAlign: 'center', fontWeight: 'medium', fontSize: 17}}>
                             2
                         </Typography>
                         <Typography variant="body1" textAlign={'center'} >
@@ -51,7 +51,7 @@ const UserPreviewCard = ({ address }) => {
                         </Typography>
                     </Grid>
                     <Grid item xs={6}>
-                        <Typography variant="body1" textAlign={'center'} sx={{fontWeight: 'medium'}}>
+                        <Typography sx={{ textAlign: 'center', fontWeight: 'medium', fontSize: 17}}>
                             €4000
                         </Typography>
                         <Typography variant="body1" textAlign={'center'}>

832f2bc7c89da87ace89c947e50c27dc6903e296
patrickcarmody <pcarmody00@gmail.com>
2023-04-09 19:03:35 +0100

    font

--- Source/client/src/index.js
+++ Source/client/src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-import '@fontsource/dm-sans';
+import '@fontsource/poppins/300.css';
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/500.css';
+import '@fontsource/poppins/700.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- Source/client/src/theme/Typography.js
+++ Source/client/src/theme/Typography.js
@@ -1,40 +1,40 @@
 const typography = {
-    fontFamily: "'DM Sans', sans-serif;",
+    fontFamily: "'Poppins', sans-serif;",
     h1: {
       fontWeight: 600,
       fontSize: '2.25rem',
       lineHeight: '2.75rem',
-      fontFamily: "'DM Sans', sans-serif;",
+      fontFamily: "'Poppins', sans-serif;",
     },
     h2: {
       fontWeight: 600,
       fontSize: '1.875rem',
       lineHeight: '2.25rem',
-      fontFamily: "'DM Sans', sans-serif;",
+      fontFamily: "'Poppins', sans-serif;",
     },
     h3: {
       fontWeight: 600,
       fontSize: '1.5rem',
       lineHeight: '1.75rem',
-      fontFamily: "'DM Sans', sans-serif;",
+      fontFamily: "'Poppins', sans-serif;",
     },
     h4: {
       fontWeight: 600,
       fontSize: '1.3125rem',
       lineHeight: '1.6rem',
-      fontFamily: "'DM Sans', sans-serif;",
+      fontFamily: "'Poppins', sans-serif;",
     },
     h5: {
       fontWeight: 600,
       fontSize: '1.125rem',
       lineHeight: '1.6rem',
-      fontFamily: "'DM Sans', sans-serif;",
+      fontFamily: "'Poppins', sans-serif;",
     },
     h6: {
       fontWeight: 600,
       fontSize: '1rem',
       lineHeight: '1.2rem',
-      fontFamily: "'DM Sans', sans-serif;",
+      fontFamily: "'Poppins', sans-serif;",
     },
     button: {
       textTransform: 'capitalize',

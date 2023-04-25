e4bc7dd483b82dc3b8679806f09aa910edf5aeb8
patrickcarmody <pcarmody00@gmail.com>
2023-04-25 12:27:43 +0100

    rework search on campaigns

--- Source/client/src/pages/campaigns/IndividualCampaigns.js
+++ Source/client/src/pages/campaigns/IndividualCampaigns.js
@@ -8,141 +8,119 @@ import CampaignCard from '../../components/CampaignCard/CampaignCard';
 import NoResults from '../../components/NoResults';
 
 const IndividualCampaigns = () => {
-
+  const [category, setCategory] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+  const [showClearSearch, setShowClearSearch] = useState(false);
+  
   const [campaigns, setCampaigns] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('');
-  const [category, setCategory] = useState('');
   const [loading, setLoading] = useState(false);
-  const [showClearButton, setShowClearButton] = useState(false);
 
   useEffect(() => {
-    setLoading(true);
-    axios.get(`/campaigns`)
-    .then((response) => {
-        setCampaigns(response.data)
-    })
-    .catch((err) => {
-        console.error(err);
-    })
-    .finally(() => setLoading(false));
-  }, []);
-
-  const handleSearch = (categoryValue) => {
-    setLoading(true);
-    axios.get(`/campaigns?title=${searchQuery}&category=${categoryValue}`)
-      .then((response) => {
-        setCampaigns(response.data);
-      })
-      .catch((err) => {
-        if (err.response && err.response.status === 404) {
-            setCampaigns([])
-          console.log('404 error');
-        } else {
-          console.error(err);
-        }
-      })
+      setLoading(true);
+      axios.get(`/campaigns`)
+      .then((response) => { setCampaigns(response.data) })
+      .catch((err) => { console.error(err) })
       .finally(() => setLoading(false));
-  };
+    }, []);
 
-  const handleClearSearch = () => {
-    setSearchQuery('');
-    setShowClearButton(false);
-    handleSearch(category);
-  };
-
-  const handleSearchInputChange = (event) => {
-    setSearchQuery(event.target.value);
-    setShowClearButton(event.target.value !== '');
-  };
-
-  const handleCategoryChange = (event) => {
-    setCategory(event.target.value);
+  const handleSearchInput = (e) => {
+      setSearchQuery(e.target.value);
   }
 
-  useEffect(() => {
-    handleSearch(category);
-  }, [searchQuery, category]); // Run handleSearch on searchQuery and category change
+  const handleCategoryChange = (e) => {
+      setCategory(e.target.value);
+      handleSearch();
+  }
 
-  if (loading) return <Loading />;
+  const handleSearch = () => {
+      setLoading(true);
+      axios.get(`/campaigns?category=${category}&title=${searchQuery}`)
+      .then((response) => { setCampaigns(response.data) })
+      .catch((err) => { console.error(err) })
+      .finally(() => setLoading(false));
+  }
 
-  const campaignsList = campaigns.length
-    ? campaigns.map((campaign) => {
-      return (
-          <CampaignCard campaign={campaign} sx={{pl: 5}}/>
+  const handleClearSearch = () => {
+      setCategory("");
+      setSearchQuery("");
+      handleSearch();
+  }
+
+  const campaignsList = campaigns.length ? campaigns.map((campaign) => {
+      return(
+          <CampaignCard campaign={campaign}/>
       )
-    })
-    : <NoResults />;
-    
-    return (
-      <PageContainer title="User Campaigns" description="user campaigns">
-        <Typography variant='h3' sx={{ marginY: 2 }}>User campaigns</Typography>
-        <Grid container paddingBottom={2} spacing={2}>
-          <Grid item xs={12} lg={4}>
-          <TextField
-            label='Search'
-            fullWidth
-            onKeyPress={(event) => {
-              if (event.key === 'Enter') {
-                handleSearch();
-              }
-            }}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position='end'>
-                  {showClearButton ? (
-                    <Clear onClick={handleClearSearch} />
+  })
+  : <NoResults/>
+
+  return(
+      <PageContainer title='User campaigns'>
+          <Typography variant='h3' sx={{ marginY: 2 }}>User campaigns</Typography>
+          <Grid container paddingBottom={2} spacing={2}>
+              <Grid item xs={12} lg={4}>
+                  <TextField
+                      label='Search'
+                      fullWidth
+                      InputProps={{
+                          endAdornment: (
+                              <InputAdornment position='end'>
+                                  {showClearSearch ? (
+                                      <Clear onClick={handleClearSearch}/>
+                                  ) : (
+                                      <Search onClick={handleSearch}/>
+                                  )}
+                              </InputAdornment>
+                          )
+                      }}
+                      value={searchQuery}
+                      onChange={handleSearchInput}
+                  />
+              </Grid>
+              <Grid item xs={12} md={4}>
+                  <Select 
+                      fullWidth
+                      onChange={handleCategoryChange}
+                      defaultValue={""}
+                  >
+                      <MenuItem value=''>All</MenuItem>
+                      <MenuItem value='animals'>Animals</MenuItem>
+                      <MenuItem value='community'>Community</MenuItem>
+                      <MenuItem value='emergencies'>Emergencies</MenuItem>
+                      <MenuItem value='environment'>Environment</MenuItem>
+                      <MenuItem value='events'>Events</MenuItem>
+                      <MenuItem value='family'>Family</MenuItem>
+                      <MenuItem value='medical'>Medical</MenuItem>
+                      <MenuItem value='sports'>Sports</MenuItem>
+                      <MenuItem value='other'>Other</MenuItem>
+                  </Select>
+              </Grid>
+          </Grid>
+          <Box>
+              <Grid container spacing={3} direction="row" pt={3} pl={2} justifyContent='center'>
+              {loading && (
+                  <Grid item xs={12}>
+                     <Loading />
+                  </Grid>
+              )}
+              {!loading && (
+              <>
+                  {campaigns.length > 0 ? (
+                      campaignsList
                   ) : (
-                    <Search onClick={handleSearch} />
+                  <Grid item xs={12}>
+                      <NoResults />
+                  </Grid>
                   )}
-                </InputAdornment>
-              )
-            }}
-            value={searchQuery}
-            onChange={handleSearchInputChange}
-          />
-          </Grid>
-          <Grid item xs={12} lg={4}></Grid>
-          <Grid item xs={12} lg={4}>
-            <Select fullWidth onChange={handleCategoryChange} defaultValue=''>
-              <MenuItem value=" ">All</MenuItem>
-              <MenuItem value='animals'>Animals</MenuItem>
-              <MenuItem value='community'>Community</MenuItem>
-              <MenuItem value='emergencies'>Emergencies</MenuItem>
-              <MenuItem value='environment'>Environment</MenuItem>
-              <MenuItem value='events'>Events</MenuItem>
-              <MenuItem value='family'>Family</MenuItem>
-              <MenuItem value='medical'>Medical</MenuItem>
-              <MenuItem value='sports'>Sports</MenuItem>
-              <MenuItem value='other'>Other</MenuItem>
-            </Select>
-          </Grid>
-        </Grid>
-    
-      <Box>
-        <Grid container spacing={3} direction="row" pt={3} pl={2} justifyContent="center">
-        {loading && (
-        <Grid item xs={12}>
-          <Loading />
-        </Grid>
-        )}
-          {!loading && (
-          <>
-          {campaigns.length > 0 ? (
-            campaignsList
-          ) : (
-          <Grid item xs={12}>
-            <NoResults />
-          </Grid>
-          )}
-          </>
-          )}
-        <Grid item xs={12} lg={12}>
-        {/* Pagination here */}
-        </Grid>
-        </Grid>
-      </Box>
+              </>
+              )}
+              <Grid item xs={12} lg={12}>
+              {/* Pagination here */}
+              </Grid>
+              </Grid>
+          </Box>
       </PageContainer>
-    );
+  )
+  
     
 };
 

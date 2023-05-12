1c6463f0680b18e9bdf20b61e9fc200b2e0eac78
patrickcarmody <pcarmody00@gmail.com>
2023-05-12 19:50:51 +0100

    fix build files

--- Build/client/src/config/index.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    // Hosted API base URL
-    // baseURL: 'https://us-central1-crowdfunder-e7a8f.cloudfunctions.net/api'
-    // Local base URL
-    baseURL: 'http://localhost:3005/'
-});
